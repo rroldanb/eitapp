4896f23b0f723a304d0f389fbde2198c8b9ec240
Ruben Roldan <ruben.roldan.b@gmail.com>
2026-07-06 13:37:44 -0400

    fix: aumentar gunicorn timeout a 120s para importación
    - El timeout default de 30s causaba WORKER TIMEOUT en import validate
    - Con 2 workers, ambos se colgaban y el sitio caía

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ python manage.py migrate --noinput
 if [ "${DJANGO_DEBUG}" = "True" ]; then
     exec python manage.py runserver 0.0.0.0:${PORT:-7860}
 else
-    exec gunicorn transito_backend.wsgi --bind 0.0.0.0:${PORT:-7860} --workers 2
+    exec gunicorn transito_backend.wsgi --bind 0.0.0.0:${PORT:-7860} --workers 2 --timeout 120
 fi
